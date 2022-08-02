3a169beb0e21c04640d6b017bfc0d05f69b7cc60
amaneth <amankassahun190@gmail.com>
2022-08-02 10:42:11 +0000

    initial commit

---
+++ format_dataset.py
@@ -0,0 +1,78 @@
+import os
+import jsonlines
+import pandas as pd
+import argparse
+
+def create_dir(output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+def export_csv_files(output_dir, df, direction='orm_eng_law'):
+    
+    create_dir(output_dir)
+    
+    filename = output_dir + '/' + direction + '.csv'
+    df.to_csv(filename, index=False)
+
+
+def export_json_files(output_dir, df, direction='orm_eng_law'):
+
+    to_be_saved = []
+    src_data = df['source_lang'].values
+    tgt_data = df['target_lang'].values
+    src_lang, tgt_lang, domain = direction.split('_')
+    N_sent = df.shape[0]
+    for s in range(N_sent):
+        text_string = {"translation": {src_lang:src_data[s], tgt_lang:tgt_data[s]}}
+        to_be_saved.append(text_string)
+
+    create_dir(output_dir)
+    filename = output_dir + '/' + direction + '.json'  
+
+    with jsonlines.open(filename, 'w') as writer:
+        writer.write_all(to_be_saved)
+        
+
+
+def parallel_translation_data(source_path, target_path, input_path):
+
+    df_source = pd.read_csv(source_path, sep=r'\n', header=None, engine='python')
+    df_target = pd.read_csv(target_path, sep=r'\n', header=None, engine = 'python')
+
+    print(len(df_source),len(df_target))
+
+    src_data, tg_data = df_source.iloc[:, 0].values, df_target.iloc[:, 0].values
+
+    df_data = pd.DataFrame(src_data, columns=['source_lang'])
+    df_data['target_lang'] = tg_data
+
+    return df_data
+
+
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input',default='.')
+    #parser.add_argument('--output', default='../csvfiles')
+    parser.add_argument('--source', required = True)
+    parser.add_argument('--target', required = True)
+    parser.add_argument('--to', default = 'csv')
+    parser.add_argument('--direction', required = True)
+
+    arguments = parser.parse_args()
+    input_dir = arguments.input
+    source_path = arguments.source
+    target_path = arguments.target
+    filetype = arguments.to
+    direction = arguments.direction
+
+    parallel_df = parallel_translation_data(source_path, target_path, input_dir)  # replace direction with whatever translation direction
+    
+    if filetype == 'json':
+        export_json_files('../jsonfiles', parallel_df, direction=direction)
+    else:
+        export_csv_files('../csvfiles', parallel_df, direction=direction)
+
